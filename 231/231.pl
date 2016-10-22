e74ddf8aa2d75f4b8b7dca643ca6e7b16c2484ae
nukosuke <nukosuke@shibuya.tech>
2016-10-22 21:55:58 +0900

    shorten print

--- 231/231.pl
+++ 231/231.pl
@@ -2,8 +2,7 @@ my $N   = <> + 0;
 for (1..$N) {
   my ($G, $D) = map { $_ + 0 } split ' ', <>;
   if (($G - 30000 * $D) * 6 >= 3000000) {
-    print "YES\n";
-    print("$_\n" x 6);
+    print("YES\n" . "$_\n" x 6);
     exit(0);
   }
 }
